1a5053d56cc0cc61bbf0fcdb46e76b5a31fa5eec
n4dz <nadzamini@gmail.com>
2020-07-07 22:23:30 -0400

    added code for back end & endpoints

--- public/seat-select/js/seat-select.js
+++ public/seat-select/js/seat-select.js
@@ -1,43 +1,44 @@
-const flightInput = document.getElementById('flight');
-const seatsDiv = document.getElementById('seats-section');
-const confirmButton = document.getElementById('confirm-button');
+const flightInput = document.getElementById("flight");
+const seatsDiv = document.getElementById("seats-section");
+const confirmButton = document.getElementById("confirm-button");
 
-let selection = '';
+let selection = "";
 
-const renderSeats = () => {
-  document.querySelector('.form-container').style.display = 'block';
+const renderSeats = (data) => {
+  document.querySelector(".form-container").style.display = "block";
 
-  const alpha = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const alpha = ["A", "B", "C", "D", "E", "F"];
   for (let r = 1; r < 11; r++) {
-    const row = document.createElement('ol');
-    row.classList.add('row');
-    row.classList.add('fuselage');
+    const row = document.createElement("ol");
+    row.classList.add("row");
+    row.classList.add("fuselage");
     seatsDiv.appendChild(row);
     for (let s = 1; s < 7; s++) {
       const seatNumber = `${r}${alpha[s - 1]}`;
-      const seat = document.createElement('li');
+      const seat = document.createElement("li");
 
       // Two types of seats to render
       const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`;
       const seatAvailable = `<li><label class="seat"><input type="radio" name="seat" value="${seatNumber}" /><span id="${seatNumber}" class="avail">${seatNumber}</span></label></li>`;
 
       // TODO: render the seat availability based on the data...
+      /* how to get available seat from data?*/
       seat.innerHTML = seatAvailable;
       row.appendChild(seat);
     }
   }
 
-  let seatMap = document.forms['seats'].elements['seat'];
+  let seatMap = document.forms["seats"].elements["seat"];
   seatMap.forEach((seat) => {
     seat.onclick = () => {
       selection = seat.value;
       seatMap.forEach((x) => {
         if (x.value !== seat.value) {
-          document.getElementById(x.value).classList.remove('selected');
+          document.getElementById(x.value).classList.remove("selected");
         }
       });
-      document.getElementById(seat.value).classList.add('selected');
-      document.getElementById('seat-number').innerText = `(${selection})`;
+      document.getElementById(seat.value).classList.add("selected");
+      document.getElementById("seat-number").innerText = `(${selection})`;
       confirmButton.disabled = false;
     };
   });
@@ -45,33 +46,40 @@ const renderSeats = () => {
 
 const toggleFormContent = (event) => {
   const flightNumber = flightInput.value;
-  console.log('toggleFormContent: ', flightNumber);
-  fetch(`/flights/${flightNumber}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-  // TODO: contact the server to get the seating availability
-  //      - only contact the server if the flight number is this format 'SA###'.
-  //      - Do I need to create an error message if the number is not valid?
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
+  //https://regex101.com/
+  const regex = /[S][A][0-9][0-9][0-9]$/g;
+  if (flightNumber.match(regex)) {
+    //console.log("toggleFormContent: ", flightNumber);
+    fetch(`/flights/${flightNumber}`)
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data[0].id);
+        renderSeats(data);
+      });
+    // TODO: contact the server to get the seating availability
+    //      - only contact the server if the flight number is this format 'SA###'.
+    //      - Do I need to create an error message if the number is not valid?
 
-  // TODO: Pass the response data to renderSeats to create the appropriate seat-type.
-  renderSeats();
+    // TODO: Pass the response data to renderSeats to create the appropriate seat-type.
+  } else {
+    alert("Not existing flight number");
+  }
 };
 
 const handleConfirmSeat = (event) => {
   event.preventDefault();
   // TODO: everything in here!
-  fetch('/users', {
-    method: 'POST',
+  fetch("/users", {
+    method: "POST",
     body: JSON.stringify({
-      givenName: document.getElementById('givenName').value,
+      givenName: document.getElementById("givenName").value,
     }),
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   });
 };
 
-flightInput.addEventListener('blur', toggleFormContent);
+flightInput.addEventListener("blur", toggleFormContent);
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { flights } = require('./test-data/flightSeating');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { flights } = require("./test-data/flightSeating");
+const users = require("./test-data/reservations");
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,24 +13,61 @@ const handleFlight = (req, res) => {
   // get all flight numbers
   const allFlights = Object.keys(flights);
   // is flightNumber in the array?
-  console.log('REAL FLIGHT: ', allFlights.includes(flightNumber));
+  //console.log("REAL FLIGHT: ", allFlights.includes(flightNumber));
+  if (!allFlights.includes(flightNumber)) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  // find specifique flights
+  //Object.entries()méthode renvoie un tableau de paires de propriétés énumérées propres à un objet donné , dans le même ordre que celui fourni par une boucle. (La seule différence importante est qu'une boucle énumère également les propriétés de la chaîne de prototypes). [key, value]for...infor...in
+  let foundFlight = null;
+  for (const [key, value] of Object.entries(flights)) {
+    if (key == flightNumber) {
+      foundFlight = value;
+    }
+  }
+
+  //response the flight informations
+  res.status(200).json(foundFlight);
+};
+
+//returns an array of flight numbers
+const handleAllFlights = (req, res) => {
+  // get all flight numbers
+  const allFlights = Object.keys(flights);
+
+  //response the flight informations
+  res.status(200).json(allFlights);
+};
+
+//returns an array of all users
+const handleAllUsers = (req, res) => {
+  let limit = req.query.limit;
+  let start = req.query.start;
+  //can't do the pagination??
+
+  //response the users informations
+  res.status(200).json(users);
 };
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
-  .get('/flights/:flightNumber', handleFlight)
-  .use((req, res) => res.send('Not Found'))
+  .get("/flights/:flightNumber", handleFlight)
+  //returns an array of flight numbers
+  .get("/flights", handleAllFlights)
+  .get("/users", handleAllUsers)
+  .use((req, res) => res.send("Not Found"))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
